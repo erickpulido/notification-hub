6ce0990e859276046efb5ba80559048888496129
erickpulido <135669450+erickpulido@users.noreply.github.com>
2026-09-19 06:21:56 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,8 +21,8 @@ if [ ! -d vendor ]; then
         -u "$(id -u):$(id -g)" \
         -v "$(pwd):/var/www/html" \
         -w /var/www/html \
-        laravelsail/php83-composer:latest \
-        composer install --ignore-platform-reqs
+        laravelsail/php84-composer:latest \
+        composer install
 else
     echo "--> Vendor directory already exists. Skipping initial composer install."
 fi
